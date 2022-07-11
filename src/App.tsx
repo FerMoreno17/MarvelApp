2871bb2b86111bf0bd9174a98aae803aefa3dd66
FerMoreno17 <fernandomoreno2007@gmail.com>
2022-07-10 21:07:25 -0300

    agrego splash y cambio iconos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './navigation/StackNavigation';
 import { store } from './redux/reducer';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <NavigationContainer>
       <Provider store={store}>
